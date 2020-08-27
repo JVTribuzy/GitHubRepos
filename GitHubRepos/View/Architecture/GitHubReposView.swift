edac0c5a50da9f31bb80c42f8176c7547860a44a
João Tribuzy <jvtribuzy@gmail.com>
2020-08-27 18:07:07 -0400

    SegmentedControl added

---
+++ GitHubRepos/View/Architecture/GitHubReposView.swift
@@ -0,0 +1,12 @@
+//
+//  GitHubReposView.swift
+//  GitHubRepos
+//
+//  Created by João Tribuzy on 27/08/20.
+//  Copyright © 2020 João Tribuzy. All rights reserved.
+//
+
+protocol GitHubReposView {
+    func layout()
+    func style()
+}
--- GitHubRepos/View/MainViewController.swift
+++ GitHubRepos/View/MainViewController.swift
@@ -7,9 +7,37 @@
 //
 
 import UIKit
+import Stevia
 
 class MainViewController: UIViewController {
+    let repoSegmentedControl: UISegmentedControl = UISegmentedControl(items: ["All", "Saved"])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        layout()
+        style()
+    }
+}
+
+extension MainViewController: GitHubReposView {
+    func layout() {
+        view.subviews{
+            repoSegmentedControl
+        }
+        
+        // repoSegmentedControl
+        repoSegmentedControl.top(8).right(16).left(16).height(30)
+        repoSegmentedControl.Top == view.safeAreaLayoutGuide.Top
+    }
+    
+    func style() {
+        
+        // repoSegmentedControl
+        repoSegmentedControl.selectedSegmentIndex = 0
+        repoSegmentedControl.layer.cornerRadius = 5.0
+        repoSegmentedControl.backgroundColor = .white
+        repoSegmentedControl.tintColor = .black
+
     }
 }
